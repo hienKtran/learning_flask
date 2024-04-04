107790054d9ff92afeaa68c36fdc5f82e6d17381
Hien Tran <hienkim426@gmail.com>
2024-04-04 14:26:38 -0500

    hien's app.py

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
 # test2 project, app.py
 # Mike Colbert 09/20/2019
+
 from flask import Flask
+
 app = Flask(__name__)
+
 @app.route('/')
-def hello_world():
-    return 'Hello world!'
+def index():
+    return 'Hello world, index'
+
+@app.route('/hien')
+def hien():
+    return 'Hello hien'
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
+
+
 
